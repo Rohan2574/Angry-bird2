f4a3c66331644a4a037c60cc65be977fb5432001
Rohan2574 <82577552+Rohan2574@users.noreply.github.com>
2021-05-05 14:02:34 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,23 @@
+class BaseClass{
+    constructor(x,y,w,h,angle){
+        var options ={
+            isStatic: false,
+            restitution:0.1,
+            friction:0.9
+        }
+        this.image=loadImage("base.png")
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w=w;
+        this.h=h;
+        World.add(world,this.body); 
+    }
+    display(){
+        imageMode(CENTER);
+        push()
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        fill("yellow");
+        image(this.image,0,0,this.w,this.h);
+        pop()   
+    }
+}
